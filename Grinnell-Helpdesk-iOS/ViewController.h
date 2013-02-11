ba0fb9a1564cc84d1639cd459f0fd7c120214dea
Colin <cjt782@gmail.com>
2013-02-11 11:26:05 -0600

    Intial setup

---
+++ Grinnell-Helpdesk-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Grinnell-Helpdesk-iOS
+//
+//  Created by Colin Tremblay on 2/11/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
