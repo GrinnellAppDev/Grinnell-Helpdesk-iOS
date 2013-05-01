694b868350733dcd115f49a380c6f2841de260e8
Colin Tremblay <tremblay@grinnell.edu>
2013-05-01 18:13:40 -0500

    Login Work - Incomplete

--- Grinnell-Helpdesk-iOS/LoginViewController.m
+++ Grinnell-Helpdesk-iOS/LoginViewController.m
@@ -13,7 +13,10 @@
 
 @end
 
-@implementation LoginViewController
+@implementation LoginViewController {
+    NSMutableData *receivedData;
+    NSStringEncoding encoding;
+}
 
 @synthesize username, password, goButton;
 
@@ -41,6 +44,39 @@
     [alertView show];
 }
 
+
+-(void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge{
+    [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+}
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    // This method is called when the server has determined that it
+    // has enough information to create the NSURLResponse.
+    // It can be called multiple times, for example in the case of a
+    // redirect, so each time we reset the data.
+    [receivedData setLength:0];
+    CFStringEncoding cfEncoding = CFStringConvertIANACharSetNameToEncoding((CFStringRef)
+                                                                           [response textEncodingName]);
+	encoding = CFStringConvertEncodingToNSStringEncoding(cfEncoding);
+    
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    // Append the new data to receivedData.
+    [receivedData appendData:data];
+}
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    
+    // inform the user
+    NSLog(@"Connection failed! Error - %@ %@", [error localizedDescription], [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]);
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    // do something with the data
+    NSLog(@"Succeeded! Received %d bytes of data",[receivedData length]);
+    NSString *dataString = [[NSString alloc] initWithData:receivedData encoding:encoding];
+    NSLog(@"%@", dataString);
+}
+
 - (void)go{
     @try{
         
@@ -48,11 +84,42 @@
             [self alertStatus:@"Please enter both Username and Password" :@"Login Failed!"];
         }
         else{
-            NSString *post =[[NSString alloc] initWithFormat:@"login_username=%@&login_password=%@",self.username.text, self.password.text];
+            NSURL *url = [NSURL URLWithString:@"https://kbox.grinnell.edu/userui/check_login.php"];
+            
+           // NSString *post =[[NSString alloc] initWithFormat:@"login_username=%@&login_password=%@",self.username.text, self.password.text];
             //NSLog(@"PostData: %@",post);
+           /* NSError *error;
+            NSURL *url = [NSURL URLWithString:@"https://kbox.grinnell.edu/"];
+            NSMutableURLRequest *urlRequest=[NSMutableURLRequest requestWithURL:url];
             
-            NSURL *url=[NSURL URLWithString:@"https://kbox.grinnell.edu/"];
             
+            NSURLResponse *resp;
+            [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:&resp error:&error];
+            NSLog(@"Response: %@", resp);
+            NSLog(@"Error: %@", error);*/
+            
+            
+            // Create the request.
+            NSURLRequest *theRequest=[NSURLRequest requestWithURL:url
+                                                      cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                  timeoutInterval:60.0];
+            
+            // create the connection with the request and start loading the data
+            NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest
+                                                                           delegate:self];
+            
+            if (theConnection){
+                // Create the NSMutableData to hold the received data.
+                // receivedData is an instance variable declared elsewhere.
+                receivedData = [NSMutableData data];
+            } else {
+                // Inform the user that the connection failed.
+                NSLog(@"Error occured");
+            }
+            
+            
+            
+            /*
             NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
             
             NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
@@ -102,7 +169,7 @@
                 if(error)
                     NSLog(@"Error: %@", error);
                 [self alertStatus:@"Connection Failed" :@"Login Failed!"];
-            }
+            }*/
         }
     }
     @catch(NSException * e){
